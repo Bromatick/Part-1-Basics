b24c17c8afd045f82fb1cc67709f005af52689c5
Niklas Ludwig <LudwNikl728@GOTVDSB.CA>
2020-09-15 10:33:36 -0400

    Add project files.

---
+++ Part 1 Basics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Part_1_Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("N   N   L"    );
+            Console.WriteLine("NN  N   L"    );
+            Console.WriteLine("N N N   L"    );
+            Console.WriteLine("N  NN   L"    );
+            Console.WriteLine("N   N   L"    );
+            Console.WriteLine("N   N   L"    );
+            Console.WriteLine("N   N   LLLLL");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            string firstName = "Niklas";
+            string favMovie = "The Matrix";
+            Console.WriteLine($"hi my name is {firstName} and my favorite movie is {favMovie}.");
+            favMovie = (favMovie.ToUpper());
+            Console.WriteLine($"{favMovie}");
+            Console.WriteLine(favMovie.Contains("THE"));
+            Console.WriteLine(favMovie.Replace("A", "@").Replace("E", "3"));
+        }
+    }
+}
